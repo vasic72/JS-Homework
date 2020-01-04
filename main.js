615516c4d7050a3e3b61cbfb2cc7a2f5db208201
vasic72 <58010543+vasic72@users.noreply.github.com>
2020-01-04 18:21:57 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,90 @@
-var Peter = 3500;
-var Jim = 5000;
-console.log(Peter > Jim);
+// TASK 1
+console.log("TASK 1");
 
-var John = 38;
-var Mike = ((38 + 8)/2)-8;
-console.log("Mike is " + Mike + " years old.");
+function Student(name, lastName, age, averageGrade) {
+	this.name = name;
+	this.lastName = lastName;
+	this.age = age; 
+	this.averageGrade = averageGrade;        
+  };
 
-var firstFlock = 78;
-var secondFlock = 57;
-firstFlock -= 12;
-secondFlock += 12;
-secondFlock -= 4;
-console.log(firstFlock >= secondFlock);
+// TASK 2
+console.log("");
+console.log("TASK 2");
+
+function Player(name, age, goals, yellowCards) {
+	this.name = name;
+	this.age = age;
+	this.goals = goals;
+	this.yellowCards = yellowCards;
+  }
+
+  var goalKeeper = new Player("Petar Borota", 32, 0, 2);
+  var defense = new Player("Slobodan Santrac", 25, 67, 13);
+  var attack = new Player("Blagoje Marjanovic", 29, 84, 23);
+
+  console.log(goalKeeper, defense, attack);
+
+// TASK 3
+console.log("");
+console.log("TASK 3");
+
+var players = [["Petar Borota", 32, 0, 2], ["Slobodan Santrac", 25, 67, 13], ["Blagoje Marjanovic", 29, 84, 23]];
+
+function PlayerLoop(player) {
+	this.name = player[0];
+	this.age = player[1];
+	this.goals = player[2];
+	this.yellowCards = player[3];
+};
+
+for (var i = 0; i < players.length; i++) {
+	var name = players[i][0].toLowerCase();
+	this[name] = new PlayerLoop(players[i]);
+
+	console.log(this[name]);
+};
+
+// TASK 4
+console.log("");
+console.log("TASK 4");
+
+function Operations(firstNumber, secondNumber, operation) {
+	this.x = firstNumber;
+	this.y = secondNumber;
+	this.func = operation;
+};
+
+var multy = function (x, y) {
+	return x * y;
+};
+
+var div = function (x, y) {
+	return x / y;
+};
+
+var add = function (x, y) {
+	return x + y;
+};
+
+var sub = function (x, y) {
+	return x - y;
+};
+
+var multiplying = new Operations(2, 3, multy);
+var dividing = new Operations(48, 4, div);
+var adding = new Operations(22, 33, add);
+var subtracting = new Operations(200, 30, sub);
+
+console.log(multiplying);
+console.log(multiplying.func(multiplying.x, multiplying.y));
+
+console.log(dividing);
+console.log(dividing.func(dividing.x, dividing.y));
+
+console.log(adding);
+console.log(adding.func(adding.x, adding.y));
+
+console.log(subtracting);
+console.log(subtracting.func(subtracting.x, subtracting.y));
+
