fd2a8dfe64a99f18c28c3f37fcf97cffb1574f61
vasic72 <58010543+vasic72@users.noreply.github.com>
2020-02-15 23:34:31 +0100

    Add files via upload

---
+++ data.js
@@ -0,0 +1,95 @@
+var teamData = { teamName : "OFK BEOGRAD",
+				logoPath : "images/logo.png",
+				players : [
+					{
+						name : "Vukasin Vranes",
+						number : 1,
+						position : "goalkeeper",
+						age : 24
+					},
+					{
+						name : "Dejan Jevtic",
+						number : 2,
+						position : "goalkeeper",
+						age : 21
+					},
+					{
+						name : "Nikola Ignjatovic",
+						number : 3,
+						position : "defense",
+						age : 28
+					},
+					{
+						name : "Zlatko Ilicic",
+						number : 4,
+						position : "defense",
+						age : 31
+					},
+					{
+						name : "Stefan Aleksic",
+						number : 5,
+						position : "defense",
+						age : 19
+					},
+					{
+						name : "Dragan Markovic",
+						number : 6,
+						position : "defense",
+						age : 25
+					},
+					{
+						name : "Djordje Zupanjac",
+						number : 7,
+						position : "center",
+						age : 28
+					},
+					{
+						name : "Predrag Petosevic",
+						number : 8,
+						position : "center",
+						age : 22
+					},
+					{
+						name : "Mateja Ristic",
+						number : 9,
+						position : "center",
+						age : 24
+					},
+					{
+						name : "Stefan Kukoljac",
+						number : 10,
+						position : "center",
+						age : 21
+					},
+					{
+						name : "Mirko Djurovic",
+						number : 11,
+						position : "center",
+						age : 27
+					},
+					{
+						name : "Milan Bacvanski",
+						number : 12,
+						position : "attack",
+						age : 29
+					},
+					{
+						name : "Stefan Jankovic",
+						number : 13,
+						position : "attack",
+						age : 31
+					},
+					{
+						name : "Aleksa Velickovic",
+						number : 14,
+						position : "attack",
+						age : 25
+					},
+					{
+						name : "Vojislav Nedeljkovic",
+						number : 15,
+						position : "attack",
+						age : 28
+					}
+				]
+};
--- main.js
+++ main.js
@@ -1,14 +1,84 @@
-var Peter = 3500;
-var Jim = 5000;
-console.log(Peter > Jim);
+let allPlayers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
+	
+let bench = []; //array of reserves
+const random = Math.round(Math.random());  // One goalkeeper to the bench
+bench[bench.length] = allPlayers[random];
+allPlayers.splice(random, 1); // removing reserve from allPlayers
 
-var John = 38;
-var Mike = ((38 + 8)/2)-8;
-console.log("Mike is " + Mike + " years old.");
+for (let i = 0; i < 3; i++) { // three players more to the bench
+const random = Math.floor(Math.random() * (allPlayers.length - 1) + 1); //  + 1 to avoid second goalkeeper to the bench
+bench[bench.length] = allPlayers[random];
+allPlayers.splice(random, 1); // removing reserve from allPlayers
+};
 
-var firstFlock = 78;
-var secondFlock = 57;
-firstFlock -= 12;
-secondFlock += 12;
-secondFlock -= 4;
-console.log(firstFlock >= secondFlock);
+const banner = document.createElement("img"); // adding banner
+const header = document.querySelector("header");
+header.appendChild(banner);
+banner.setAttribute("src", "images/banner.jpg");
+banner.className = "banner"; 
+
+const logo = document.createElement("img"); // adding logo
+const headerLogo = document.querySelector("header div");
+headerLogo.appendChild(logo);
+logo.setAttribute("src", teamData.logoPath);
+logo.className = ("logo");
+
+const title = document.createElement("h1"); // adding team name
+const  mainTitle= document.querySelector("main");
+mainTitle.prepend(title);
+title.textContent = teamData.teamName;
+title.className = ("title");
+
+document.getElementById("mainTeam").textContent = "MAIN TEAM"; // subheader
+document.getElementById("reserveTeam").textContent = "RESERVE PLAYERS"; // subheader
+
+for (let i = 0; i < 11; i++) { //adding main team
+	let player = document.createElement("article");
+	let  main = document.querySelector("div.main");
+	main.appendChild(player);
+	player.className = ("m" + i);
+
+	let index = allPlayers[i];
+
+	let strImage = `<img class='article' src='images/${teamData.players[index].number}.jpg'>`;
+	let strName = `<h3>${teamData.players[index].name}</h3>`;
+	let strNumber = `<br> Number: ${teamData.players[index].number}`;
+	let strPosition = `<br> Position: ${teamData.players[index].position}`;
+	let strAge = `<br> Age: ${teamData.players[index].age}`;
+
+	player.innerHTML = strImage + strName + strNumber + strPosition + strAge;
+};
+
+for (let i = 0; i < 4; i++) { //adding reserve
+	let player = document.createElement("article");
+	let  main = document.querySelector("div.reserve");
+	main.appendChild(player);
+	player.className = ("r" + i);
+
+	let index = bench[i];
+
+	let strImage = `<img class='article' src='images/${teamData.players[index].number}.jpg'>`;
+	let strName = `<h3>${teamData.players[index].name}</h3>`;
+	let strNumber = `<br> Number: ${teamData.players[index].number}`;
+	let strPosition = `<br> Position: ${teamData.players[index].position}`;
+	let strAge = `<br> Age: ${teamData.players[index].age}`;
+
+	player.innerHTML = strImage + strName + strNumber + strPosition + strAge;
+};
+
+let change = ()  => {
+	let fromMain = Math.floor(Math.random() * 11);
+	let fromReserve = Math.floor(Math.random() * 4);
+	let temp = allPlayers[fromMain];
+	allPlayers[fromMain] = bench[fromReserve];
+	bench[fromReserve] = temp;
+
+	let toReserve = document.querySelector(`.m${fromMain}`);
+	let toMain = document.querySelector(`.r${fromReserve}`);
+
+	let tempSubst = toReserve.innerHTML;
+	toReserve.innerHTML = toMain.innerHTML;
+	toMain.innerHTML = tempSubst;
+};
+
+setInterval(change, 3000);
